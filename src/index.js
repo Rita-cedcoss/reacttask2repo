49e9bfcec95f5916a70178a7dc839e3ee4d1f667
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-25 17:32:32 +0530

    factorial task completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+var i, fact=1;
+function factorial(num)
+{
+    for(i=1;i<=num;i++)
+    {
+      fact=fact*i;
+    }
+    return fact;
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<div><p>{factorial(7)}</p></div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
